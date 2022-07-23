48248fb25aed1091b4599f839e3135ddada7e2b3
Joseph McPartland <joemcpartland@gmail.com>
2022-07-23 14:06:46 -0600

    commit tree 5df876f0b01a29625499587e121103885fe85777

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts '🌱 Seeding students...'
+
+Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
+Student.create(first_name: 'Idris', last_name: 'Elba', grade: 105)
+Student.create(first_name: 'Vanessa', last_name: 'Kirby', grade: 85)
+Student.create(first_name: 'Jason', last_name: 'Statham', grade: 70)
+
+puts '✅ Done seeding'
